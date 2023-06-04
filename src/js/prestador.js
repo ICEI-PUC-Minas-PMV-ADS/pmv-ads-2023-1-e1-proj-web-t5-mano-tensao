f55f55a6586981ead596cf2f83bf4fc1ce3a193a
THIAGO SOUZA DIAS <thiagodiasjornalista@gmail.com>
2023-06-04 18:10:51 -0300

    Inserção de criação de perfil de cliente

--- src/js/prestador.js
+++ src/js/prestador.js
@@ -1,8 +1,28 @@
-$(function(){
-	var operacao = "A"; //"A"=Adição; "E"=Edição
-	var indice_selecionado = -1; //Índice do item selecionado na lista
-	var tbPrestador = localStorage.getItem("tbPrestador");// Recupera os dados armazenados
-	tbPrestador = JSON.parse(tbPrestador); // Converte string para objeto
-	if(tbPrestador == null) // Caso não haja conteúdo, iniciamos um vetor vazio
-	tbPrestador = [];
-});
+function adicPerfil(id, nome_perfil, cidade, bairros, recursos) {
+    var usuarioIndex = db_usuarios.usuarios.findIndex(function(usuario) {
+        return usuario.id === id;
+    });
+
+    if (usuarioIndex !== -1) {
+        var usuario = db_usuarios.usuarios[usuarioIndex];
+
+        var perfil = {
+            "nome_perfil": nome_perfil,
+            "recursos_financeiros": recursos,
+            "cidade": {
+                "nome": cidade,
+                "bairros": bairros.split(","),
+            }
+        };
+        usuario.perfis_cliente.push(perfil);
+
+        db_usuarios.usuarios[usuarioIndex] = usuario; 
+		usuarioCorrente = usuario;
+        sessionStorage.setItem('usuarioCorrente', JSON.stringify(usuario));
+        localStorage.setItem('db_usuarios', JSON.stringify(db_usuarios));
+
+        return true;
+    } else {
+        return false; 
+    }
+}
